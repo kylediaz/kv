0b53d96cc89113960bf43e7cba13eb2eab43936f
Kyle Diaz <dev@kylediaz.com>
2025-02-19 18:32:59 -0500

    Implement basic TCP listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_connection(stream: &mut std::net::TcpStream) {
+    let mut buffer = [0; 1024];
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                let response = "+PONG\r\n";
+                stream.write(response.as_bytes()).unwrap();
+                stream.flush().unwrap();
+            }
+            Ok(_) => {
+                println!("connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                break;
+            }
+        }
+    }
 }
