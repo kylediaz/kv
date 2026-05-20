d4fdb78f5955138186764bed56fb6d82327a4f09
Kyle Diaz <kyle@trychroma.com>
2026-05-20 14:39:24 -0700

    test: seeded random quicklist bench

--- benches/quicklist.rs
+++ benches/quicklist.rs
@@ -1,13 +1,14 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 
 use kv::quicklist::{Dequeue, Quicklist};
+use rand::{Rng, SeedableRng, rngs::ChaCha8Rng};
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("quicklist i32", |b| {
+    c.bench_function("quicklist i32 stairs", |b| {
         b.iter(|| {
             let mut list: Quicklist<i32> = Quicklist::new();
-            // This will generate a structure ...7531246...
+            // This will generate a structure ...7 5 3 1 2 4 6...
             for n in 1..=1000 {
                 if n % 2 == 0 {
                     list.rpush(black_box(n));
@@ -23,6 +24,31 @@ fn criterion_benchmark(c: &mut Criterion) {
                     v = list.lpop();
                 }
                 assert_eq!(v.unwrap(), n);
+            }
+        })
+    });
+
+    c.bench_function("quicklist u32 seeded random", |b| {
+        b.iter(|| {
+            let mut rng = ChaCha8Rng::seed_from_u64(10);
+            let mut list: Quicklist<u32> = Quicklist::new();
+            for n in 1..=10_000 {
+                let action = rng.next_u64();
+                // Use the 32 least significant bits to determine the action
+                // Use the other 32 bits as the potentially pushed value
+                let left: bool = (action & 1) == 1;
+                // 75% chance of pushing
+                let push: bool = (action & 0b110) != 0;
+                let value = (action >> 32) as u32;
+                if left && push {
+                    list.lpush(black_box(value));
+                } else if !left && push {
+                    list.rpush(black_box(n));
+                } else if left && !push {
+                    list.lpop();
+                } else if !left && !push {
+                    list.rpop();
+                }
             }
         })
     });
